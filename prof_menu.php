a3f3ce94219a1df960ba116e14f6a2f7bef42610
TheTeaRex <thetearex10@gmail.com>
2016-04-21 08:07:03 +0000

    added template on prof menu

---
+++ prof_menu.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Forms with jQuery Mobile</title>
+    <link rel="stylesheet" href="/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css" />
+    <script src="/jquery.mobile-1.4.5/jquery.min.js"></script>
+    <script src="/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    <script src="js/prof_menu.js"></script>
+  </head>
+  <body>
+    <!-- menu -->
+    <div data-role="page" id="menu">
+      <div data-role="header">
+        <h1>Professor's Menu</h1>
+      </div>
+
+      <div data-role="content">
+        <a href="#addaclass" data-transition="pop" class="ui-btn ui-shadow ui-corner-all">Add a Class</a>
+        <a href="#editaclass" class="ui-btn ui-shadow ui-corner-all">Edit a Class</a>
+        <a href="#editstudentscore" class="ui-btn ui-shadow ui-corner-all">Edit a Student's Score</a>
+      </div>
+
+    </div>
+    <!-- end of the menu -->
+
+    <!-- add a class -->
+    <div data-role="page" id="addaclass">
+      <div data-role="header">
+        <a href="#menu" data-transition="slide" data-icon="back">Back</a>
+        <h1>Add a Class</h1>
+      </div>
+
+      <div data-role="content">
+        <form name="addclass_form" method ="post" action="backend.php">
+          <label>
+            Class Name:
+            <input type="text" name="classname" id="classname">
+          </label>
+        </form>
+      </div>
+
+    </div>
+    <!-- end of the add a class -->
+  </body>
+</html>
+
