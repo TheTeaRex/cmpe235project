887e9f7e577942f8fcf56e708dc1e20299908e63
TheTeaRex <thetearex10@gmail.com>
2016-04-22 07:51:47 +0000

    added some more stuff

--- prof_menu.php
+++ prof_menu.php
@@ -3,9 +3,10 @@
   <head>
     <title>Forms with jQuery Mobile</title>
     <link rel='stylesheet' href='/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css' />
+    <link rel='stylesheet' href='/css/prof_menu.css' />
     <script src='/jquery.mobile-1.4.5/jquery.min.js'></script>
     <script src='/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js'></script>
-    <script src='js/prof_menu.js'></script>
+    <script src='/js/prof_menu.js'></script>
   </head>
   <body>
     <!-- menu -->
@@ -32,20 +33,22 @@
 
       <div data-role='content'>
         <form name='addclass_form' method ='post' action='backend.php'>
-          <label>
+          <div>
             Class Name:
-            <input type='text' name='classname' id='classname' placeholder='Enter Class Name Here'>
-          </label>
-          <label>
-            <button type='button' class='ui-btn ui-shadow ui-corner-all'>Add another item</button> 
-          </label>
+            <input type='text' name='classname' id='classname' placeholder='Enter Class Name'>
+          </div>
+          <div class='ui-grid-b'>
+            <div class='ui-block-a big'><input type='text' name='item' id='item' placeholder='Enter an Item' ></div>
+            <div class='ui-block-b mid'><input type='text' name='percentage' id='percentage' placeholder='Enter the Percentage'></div>
+            <div class='ui-block-c sma'><button type='button' class='ui-btn ui-shadow ui-corner-all' data-mini='true'>Remove</button></div>
+          </div>
+          <div>
+            <button type='button' class='ui-btn ui-shadow ui-corner-all'>Add another item</button>
+          </div>
           <br>
-          <label>
-            <input type='submit' class='ui-btn ui-btn-b ui-shadow ui-corner-all' value='Create Class'> 
-          </label>
+          <input type='submit' class='ui-btn ui-btn-b ui-shadow ui-corner-all' value='Create Class'>
         </form>
       </div>
-
     </div>
     <!-- end of the add a class -->
   </body>
