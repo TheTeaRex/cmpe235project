dfc4c49bf749182949b293fc6b9e9b38e21f2f91
TheTeaRex <thetearex10@gmail.com>
2016-05-03 05:15:10 +0000

    confirmation page link works now

--- addclass_confirmation.php
+++ addclass_confirmation.php
@@ -5,75 +5,81 @@
     <link rel='stylesheet' href='/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css' />
     <script src='/jquery.mobile-1.4.5/jquery.min.js'></script>
     <script src='/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js'></script>
+    <script>
+      function linkTo(){
+        document.location.href = 'prof_menu.php';
+      }
+    </script>
   </head>
-<body>
-  <?php
+  <body>
+    <?php
 
-    if ($_POST[classname] != '') {
+      if ($_POST[classname] != '') {
 
-      $ini = parse_ini_file('config.ini');
-      $server = $ini['server'];
-      $username = $ini['username'];
-      $password = $ini['password'];
-      $database = $ini['database'];
+        $ini = parse_ini_file('config.ini');
+        $server = $ini['server'];
+        $username = $ini['username'];
+        $password = $ini['password'];
+        $database = $ini['database'];
 
-      // create connection
-      $conn = new mysqli($server, $username, $password, $database);
+        // create connection
+        $conn = new mysqli($server, $username, $password, $database);
 
-      // check connection
-      if ($conn->connect_error) {
-        echo '<script>';
-        echo "alert('Not able to connect to the database at the moment, please try again later.')";
-        echo '</script>';
-        die('Connection failed: '.$conn->connect_error);
+        // check connection
+        if ($conn->connect_error) {
+          echo '<script>';
+          echo "alert('Not able to connect to the database at the moment, please try again later.')";
+          echo '</script>';
+          die('Connection failed: '.$conn->connect_error);
+        }
+
+        $size = $_POST[compSize];
+        $sql = "insert into classes (className";
+
+        for ($x = 0; $x < $size; $x++) {
+          $sql .= ", comp" . $x . ", mpoint" . $x . ", percentage" . $x;
+        }
+
+        $sql .= ") values ('".$_POST[classname]."'";
+
+        for ($x = 0; $x < $size; $x++) {
+          $comp = $_POST[component.$x];
+          $point = $_POST[point.$x];
+          $per = $_POST[per.$x];
+          $sql .= ", '" . $comp . "', '" . $point . "', '" . $per."'";
+        }
+
+        $sql .= ")";
+
+        //echo $sql;
+        //echo '<br><br>';
+
+        if ($conn->query($sql) === TRUE) {
+          //echo 'New record created successfully';
+          echo '<script>';
+          echo "alert('New record created successfully');";
+          echo "document.location.href = 'prof_menu.php';";
+          echo '</script>';
+        } elseif ($conn->errno == 1062) {
+          //echo 'Error: ' . $sql.'<br>'. $conn->error;
+          echo '<script>';
+          echo "document.location.href = 'prof_menu.php#addaclass';";
+          echo "alert('Class name already exists in the database.');";
+          echo '</script>';
+        } else {
+          //echo 'Error: ' . $sql.'<br>'. $conn->error;
+          echo '<script>';
+          echo "alert('Uh Oh, I hit an error.');";
+          echo "document.location.href = 'prof_menu.php';";
+          echo '</script>';
+        }
+
+        $conn->close();
+
       }
 
-      $size = $_POST[compSize];
-      $sql = "insert into classes (className";
+    ?>
+    <a href='#' onClick='linkTo()' data-transition='pop' class='ui-btn ui-shadow ui-corner-all'>Go Back</a>
 
-      for ($x = 0; $x < $size; $x++) {
-        $sql .= ", comp" . $x . ", mpoint" . $x . ", percentage" . $x;
-      }
-
-      $sql .= ") values ('".$_POST[classname]."'";
-
-      for ($x = 0; $x < $size; $x++) {
-        $comp = $_POST[component.$x];
-        $point = $_POST[point.$x];
-        $per = $_POST[per.$x];
-        $sql .= ", '" . $comp . "', '" . $point . "', '" . $per."'";
-      }
-
-      $sql .= ")";
-
-      //echo $sql;
-      //echo '<br><br>';
-
-      if ($conn->query($sql) === TRUE) {
-        //echo 'New record created successfully';
-        echo '<script>';
-        echo "alert('New record created successfully');";
-        echo "document.location.href = 'prof_menu.php';";
-        echo '</script>';
-      } elseif ($conn->errno == 1062) {
-        //echo 'Error: ' . $sql.'<br>'. $conn->error;
-        echo '<script>';
-        echo "document.location.href = 'prof_menu.php#addaclass';";
-        echo "alert('Class name already exists in the database.');";
-        echo '</script>';
-      } else {
-        //echo 'Error: ' . $sql.'<br>'. $conn->error;
-        echo '<script>';
-        echo "alert('Uh Oh, I hit an error.');";
-        echo "document.location.href = 'prof_menu.php';";
-        echo '</script>';
-      }
-
-      $conn->close();
-
-    }
-
-    echo "<a href='#menu' data-transition='pop' class='ui-btn ui-shadow ui-corner-all'>Go Back</a>";
-  ?>
-</body>
+  </body>
 </html>
