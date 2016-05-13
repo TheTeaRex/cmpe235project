122c0d834bae2e196e26a0f1f912798d08c4cef1
TheTeaRex <thetearex10@gmail.com>
2016-05-12 18:05:52 -0700

    improvement for getClasses

--- getClasses.php
+++ getClasses.php
@@ -22,8 +22,9 @@
     $newarray[] = $row;
   }
 
-  echo json_encode($newarray);
-
   $result->free();
   $conn->close();
+
+  header('Content-Type: application/json');
+  echo json_encode($newarray);
 ?>
