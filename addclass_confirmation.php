e49d29df14bc11c95a1b021753108989232ad9fb
TheTeaRex <thetearex10@gmail.com>
2016-05-12 17:48:55 -0700

    copied the php from server

--- addclass_confirmation.php
+++ addclass_confirmation.php
@@ -1,57 +1,58 @@
-<?php
 
-  if ($_POST[classname] != '') {
+    <?php
 
-    $ini = parse_ini_file('config.ini');
-    $server = $ini['server'];
-    $username = $ini['username'];
-    $password = $ini['password'];
-    $database = $ini['database'];
+      if ($_POST[classname] != '') {
 
-    // create connection
-    $conn = new mysqli($server, $username, $password, $database);
+        $ini = parse_ini_file('config.ini');
+        $server = $ini['server'];
+        $username = $ini['username'];
+        $password = $ini['password'];
+        $database = $ini['database'];
 
-    // check connection
-    if ($conn->connect_error) {
-      echo '<script>';
-      echo "alert('Not able to connect to the database at the moment, please try again later.')";
-      echo '</script>';
-      die('Connection failed: '.$conn->connect_error);
-    }
+        // create connection
+        $conn = new mysqli($server, $username, $password, $database);
 
-    $size = $_POST[compSize];
-    $sql = "insert into classes (className";
+        // check connection
+        if ($conn->connect_error) {
+          echo '<script>';
+          echo "alert('Not able to connect to the database at the moment, please try again later.')";
+          echo '</script>';
+          die('Connection failed: '.$conn->connect_error);
+        }
 
-    for ($x = 0; $x < $size; $x++) {
-      $sql .= ", comp" . $x . ", mpoint" . $x . ", percentage" . $x;
-    }
+        $size = $_POST[compSize];
+        $sql = "insert into classes (className";
 
-    $sql .= ") values ('".$_POST[classname]."'";
+        for ($x = 0; $x < $size; $x++) {
+          $sql .= ", comp" . $x . ", mpoint" . $x . ", percentage" . $x;
+        }
 
-    for ($x = 0; $x < $size; $x++) {
-      $comp = $_POST[component.$x];
-      $point = $_POST[point.$x];
-      $per = $_POST[per.$x];
-      $sql .= ", '" . $comp . "', '" . $point . "', '" . $per."'";
-    }
+        $sql .= ") values ('".$_POST[classname]."'";
 
-    $sql .= ")";
+        for ($x = 0; $x < $size; $x++) {
+          $comp = $_POST[component.$x];
+          $point = $_POST[point.$x];
+          $per = $_POST[per.$x];
+          $sql .= ", '" . $comp . "', '" . $point . "', '" . $per."'";
+        }
 
-    //echo $sql;
-    //echo '<br><br>';
+        $sql .= ")";
 
-    if ($conn->query($sql) === TRUE) {
-      $response_array["status"] = "success";
-    } elseif ($conn->errno == 1062) {
-      $response_array["status"] = "dup";
-    } else {
-      $response_array["status"] = "error";
-    }
+        //echo $sql;
+        //echo '<br><br>';
 
-    $conn->close();
+        if ($conn->query($sql) === TRUE) {
+          $response_array["status"] = "success";
+        } elseif ($conn->errno == 1062) {
+          $response_array["status"] = "dup";
+        } else {
+          $response_array["status"] = "error";  
+        }
 
-    header('Content-Type: application/json');
-    echo json_encode($response_array);
-  } ;
+        $conn->close();
 
-?>
+        header('Content-Type: application/json');
+        echo json_encode($response_array);
+      } ;
+
+    ?>
